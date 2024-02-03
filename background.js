f863941da30990201e976d05d2c32fab5607db82
Hatzilu <taazrooli@gmail.com>
2024-02-04 01:18:55 +0200

    lint, general fixes

--- background.js
+++ background.js
@@ -1,83 +1,47 @@
-import {arrayBufferToString, concatenateArrayBuffers } from './lib/buffer.js'
-/**
- * 
- * @param {Element} elem 
- */
-function doubleClick(elem) {
-    elem.dispatchEvent(new Event("dblclick", {bubbles: true}));
-}
-
-let inventoryResponse;
-
-const timer = setInterval(() =>{
-    if (inventoryResponse && inventoryResponse?.success) {
-        clearInterval(timer)
-
-     }
-    
-    }, 2000)
-
+import { arrayBufferToString, concatenateArrayBuffers } from './lib/buffer.js';
 
 function logURL(details) {
-    console.log({details})
-    if (!details.url.includes('/inventory/json/')) return;
-    document.body.style.border = "5px solid red";
+	console.log({ details });
+	if (!details.url.includes('/inventory/json/')) return;
+	document.body.style.border = '5px solid red';
 
-    let filter = browser.webRequest.filterResponseData(details.requestId)
-    console.log({filter})
-    
-    let chunks = [];
-    
-    filter.onstart = (event) => {
-        console.log("started", {event});
-      };
-    
-      filter.ondata = (event) => { 
-        if (!event.data) return;
-        chunks.push(event.data);
-        filter.write(event.data);
-      };
-    
-      filter.onstop = (event) => {
-        filter.close();
-        console.log('stopped', {event});   
-        const buffer = concatenateArrayBuffers(chunks);
-        const string = arrayBufferToString(buffer);
-        try {
-            inventoryResponse = JSON.parse(string)
-            browser.webRequest.onBeforeRequest.removeListener(logURL);
-        }
-        catch (error) {
-            console.error('failed to turn inventory response to string', {error, string: buffer, chunks})
-        }
-      };
+	let filter = browser.webRequest.filterResponseData(details.requestId);
+	console.log({ filter });
+
+	let chunks = [];
+
+	filter.onstart = (event) => {
+		console.log('started', { event });
+	};
+
+	filter.ondata = (event) => {
+		if (!event.data) return;
+		chunks.push(event.data);
+		filter.write(event.data);
+	};
+
+	filter.onstop = (event) => {
+		filter.close();
+		console.log('stopped', { event });
+		const buffer = concatenateArrayBuffers(chunks);
+		const string = arrayBufferToString(buffer);
+		try {
+			let inventoryResponse = JSON.parse(string);
+			browser.runtime.sendMessage(undefined, { action: 'getSharedData', message: inventoryResponse });
+			browser.webRequest.onBeforeRequest.removeListener(logURL);
+
+			// .then(() => console.log('sent')).catch(console.error);
+		} catch (error) {
+			console.error('failed to turn inventory response to string', { error, string: buffer, chunks });
+		}
+	};
 }
 
-/**
- * @returns {{userId: string; appId: string; contextid: string}}
- */
-function getUserDataFromHTML(){
-    /**
-     * @type {NodeListOf<HTMLDivElement>}
-     */
-    const inventories = document.querySelectorAll('.inventory_ctn');
-    console.log({inventories});
-    const activeInventory = [...inventories].find(a =>a.style.display !== 'none');
-    console.log({activeInventory});
-
-    const [, userId, appId, contextid] = activeInventory.id.split('_');
-    
-    return {userId, appId, contextid}
-}
-
-async function getItems(userId, appId, contextId) {
-    
-}
-
-
-browser.webRequest.onBeforeSendHeaders.addListener(logURL, {
-    urls: ["https://steamcommunity.com/*"],
-    types: ["xmlhttprequest"]
-  }, ["blocking"]);
-
-
+browser.webRequest.onBeforeSendHeaders.addListener(
+	logURL,
+	{
+		urls: ['https://steamcommunity.com/*'],
+		types: ['xmlhttprequest'],
+	},
+	['blocking'],
+);
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 // setTimeout(() => window.g_ActiveInventory.rgItemElements,2500)
 // console.log(window)
 // console.log({inventory: window.g_ActiveInventory})
@@ -9,3 +7,17 @@
 //         console.log('window.',name);
 //     }
 // }
+// const timer = setInterval(() =>{
+//     if (inventoryResponse && inventoryResponse?.success) {
+//         clearInterval(timer)
+//         console.log({inventoryResponse});
+//      }
+
+//     }, 2000)
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	console.log({ request, sender });
+	if (request.action === 'getSharedData') {
+		// Send the shared data to the content script
+		//   sendResponse({ data: sharedData });
+	}
+});
