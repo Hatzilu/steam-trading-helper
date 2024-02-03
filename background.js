21dd135c1a8ab4824c426cc510f777ef6149d44b
Hatzilu <taazrooli@gmail.com>
2024-02-03 23:58:42 +0200

    support es6 import/export

--- background.js
+++ background.js
@@ -1,32 +1,4 @@
-
-
-
-// Helper function to concatenate multiple ArrayBuffers
-function concatenateArrayBuffers(buffers) {
-    console.log(buffers);
-    let totalLength = buffers.reduce((acc, buffer) => {
-        console.log({acc, buffer});
-        if (typeof buffer?.byteLength === 'number') {
-            return acc + buffer.byteLength
-        }
-        else {console.log('not  a number', {buffer}); return acc;}
-    }, 0);
-    let concatenatedBuffer = new Uint8Array(totalLength);
-    let offset = 0;
-  
-    buffers.forEach(buffer => {
-      concatenatedBuffer.set(new Uint8Array(buffer), offset);
-      offset += buffer.byteLength;
-    });
-  
-    return concatenatedBuffer.buffer;
-}
-
-// Helper function to convert ArrayBuffer to string
-function arrayBufferToString(buffer) {
-    return new TextDecoder().decode(new Uint8Array(buffer));
-}
-
+import {arrayBufferToString, concatenateArrayBuffers } from './lib/buffer.js'
 /**
  * 
  * @param {Element} elem 
@@ -35,14 +7,12 @@ function doubleClick(elem) {
     elem.dispatchEvent(new Event("dblclick", {bubbles: true}));
 }
 
-let filter;
-
 let inventoryResponse;
 
 const timer = setInterval(() =>{
     if (inventoryResponse && inventoryResponse?.success) {
-         console.log(inventoryResponse)
         clearInterval(timer)
+
      }
     
     }, 2000)
@@ -52,20 +22,23 @@ function logURL(details) {
     console.log({details})
     if (!details.url.includes('/inventory/json/')) return;
     document.body.style.border = "5px solid red";
-    requestId = details.requestId;
-    console.log({requestId});
-    filter = browser.webRequest.filterResponseData(details.requestId)
+
+    let filter = browser.webRequest.filterResponseData(details.requestId)
     console.log({filter})
+    
     let chunks = [];
+    
     filter.onstart = (event) => {
         console.log("started", {event});
       };
-    filter.ondata = (event) => { 
+    
+      filter.ondata = (event) => { 
         if (!event.data) return;
         chunks.push(event.data);
         filter.write(event.data);
       };
-    filter.onstop = (event) => {
+    
+      filter.onstop = (event) => {
         filter.close();
         console.log('stopped', {event});   
         const buffer = concatenateArrayBuffers(chunks);
